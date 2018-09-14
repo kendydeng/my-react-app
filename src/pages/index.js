9a2e6c1f4d183c38c1978383ba01361124edd1be
Yame Design <yamedesign@YamedeiMac.local>
2018-09-14 17:29:06 +0800

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <Link to="/page-2/">Watch the button</Link>
   </div>
 )
 
